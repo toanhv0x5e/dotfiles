997a0826d44dc63b5e7a960987dc5ae2ceea33c6
toanhv0x5e <toanhv.vietnam@gmail.com>
2021-09-15 08:43:58 +0700

    Separate the agent install script

--- install.sh
+++ install.sh
@@ -125,21 +125,6 @@ vboxversion=$(wget -qO - https://download.virtualbox.org/virtualbox/LATEST.TXT)
 wget "https://download.virtualbox.org/virtualbox/${vboxversion}/Oracle_VM_VirtualBox_Extension_Pack-${vboxversion}.vbox-extpack"
 yes | sudo vboxmanage extpack install --replace Oracle_VM_VirtualBox_Extension_Pack-${vboxversion}.vbox-extpack
 
-# Drata Agent
-cd $HOME/Downloads/
-[ -f "$HOME/Downloads/drata-agent-3.0.0.deb" ] || wget https://cdn.drata.com/agent/dist/linux/drata-agent-3.0.0.deb
-sudo apt install ./drata-agent-3.0.0.deb
-
-# JumCloud Agent
-read -sp 'Connect Key: ' _connect_key
-if [ -n ${_connect_key} ] && [ `expr "$_connect_key" : '[0-9a-fA-F]\{32\}\|[0-9a-fA-F]\{40\}'` -eq ${#_connect_key} ]; then
-  curl --tlsv1.2 --silent --show-error --header \
-  'x-connect-key: ${_connect_key}' \
-  https://kickstart.jumpcloud.com/Kickstart | sudo bash
-else
-  echo -e "\nJumpCloud install failed. Manual install is required." && exit 1
-fi
-
 # Remove existing file
 [ -f "$HOME/.bashrc" ] && rm $HOME/.bashrc
 [ -f "$HOME/.bash_logout" ] && rm $HOME/.bash_logout
---
+++ scripts/scripts/agent-install.sh
@@ -0,0 +1,14 @@
+# Drata Agent
+cd $HOME/Downloads/
+[ -f "$HOME/Downloads/drata-agent-3.0.0.deb" ] || wget https://cdn.drata.com/agent/dist/linux/drata-agent-3.0.0.deb
+sudo apt install ./drata-agent-3.0.0.deb
+
+# JumCloud Agent
+read -sp 'Connect Key: ' _connect_key
+if [ -n ${_connect_key} ] && [ `expr "$_connect_key" : '[0-9a-fA-F]\{32\}\|[0-9a-fA-F]\{40\}'` -eq ${#_connect_key} ]; then
+  curl --tlsv1.2 --silent --show-error --header \
+  'x-connect-key: ${_connect_key}' \
+  https://kickstart.jumpcloud.com/Kickstart | sudo bash
+else
+  echo -e "\nJumpCloud install failed. Manual install is required." && exit 1
+fi
