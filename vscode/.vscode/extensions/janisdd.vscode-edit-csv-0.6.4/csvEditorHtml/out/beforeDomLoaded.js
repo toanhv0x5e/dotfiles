ea98f948a51b803e10bb1d3abd7b92db1e5d9a80
Toan Ha <toanhv.vietnam@gmail.com>
2022-01-07 01:10:23 +0700

    Delete vscode directory

--- vscode/.vscode/extensions/janisdd.vscode-edit-csv-0.6.4/csvEditorHtml/out/beforeDomLoaded.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-document.documentElement.style.setProperty('--extension-options-bar-display', (initialConfig === null || initialConfig === void 0 ? void 0 : initialConfig.optionsBarAppearance) === "collapsed" ? `none` : `block`);
-document.documentElement.style.setProperty('--extension-side-panel-display', (initialConfig === null || initialConfig === void 0 ? void 0 : initialConfig.sidePanelAppearance) === "collapsed" ? `none` : `flex`);
-document.documentElement.style.setProperty('--extension-side-panel-expand-icon-display', (initialConfig === null || initialConfig === void 0 ? void 0 : initialConfig.sidePanelAppearance) === "collapsed" ? `block` : `none`);
-document.documentElement.style.setProperty('--extension-side-panel-collapse-icon-display', (initialConfig === null || initialConfig === void 0 ? void 0 : initialConfig.sidePanelAppearance) === "collapsed" ? `none` : `block`);
-//# sourceMappingURL=beforeDomLoaded.js.map
--- vscode/.vscode/extensions/janisdd.vscode-edit-csv-0.6.4/thirdParty/papaparse/info.md
+++
@@ -1,3 +0,0 @@
-source: https://github.com/mholt/PapaParse
-version: 5.0.0 (modified)
-
