437d3f5af509fcb63d48670f6ccace46c1e85f16
toanhv0x5e <toanhv.vietnam@gmail.com>
2024-03-01 23:24:14 +0700

    Merge new local updates

--- bash/.bash_profile
+++ bash/.bash_profile
@@ -5,3 +5,10 @@ if [ -n "$BASH_VERSION" -a -n "$PS1" ]; then
     . "$HOME/.bashrc"
     fi
 fi
+
+function gam() { "/home/toan/bin/gam/gam" "$@" ; }
+export GAMCFGDIR="/home/toan/GAMConfig"
+#alias gam="/home/toan/bin/gamadv-xtd3/gam"
+alias gam3="/home/toan/bin/gamadv-xtd3/gam"
+alias gam="/home/toan/bin/gamadv-xtd3/gam"
+export PATH=/home/toan/program-files/hawk-3.5.0:/home/toan/perl5/bin:/home/toan/.ebcli-virtual-env/executables:/home/toan/perl5/bin:/home/toan/.ebcli-virtual-env/executables:/home/toan/.local/bin:/home/toan/bin:/home/toan/.nvm/versions/node/v14.21.3/bin:/home/toan/perl5/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/snap/bin:/usr/local/script:/home/toan/bin:/home/toan/calibre-bin/calibre:/usr/local/script:/home/toan/bin:/home/toan/calibre-bin/calibre
--- scripts/scripts/agent-install.sh
+++ scripts/scripts/agent-install.sh
@@ -6,11 +6,11 @@ if [ "$(which drata-agent)" = "" ]; then
 fi
 
 # JumpCloud Agent
-read -sp 'Connect Key: ' _connect_key
-if [ -n ${_connect_key} ] && [ `expr "$_connect_key" : '[0-9a-fA-F]\{32\}\|[0-9a-fA-F]\{40\}'` -eq ${#_connect_key} ]; then
-  curl --tlsv1.2 --silent --show-error --header \
-  "x-connect-key: ${_connect_key}" \
-  https://kickstart.jumpcloud.com/Kickstart | sudo bash
-else
-  echo -e "\nJumpCloud install failed. Manual install is required." && exit 1
-fi
+#read -sp 'Connect Key: ' _connect_key
+#if [ -n ${_connect_key} ] && [ `expr "$_connect_key" : '[0-9a-fA-F]\{32\}\|[0-9a-fA-F]\{40\}'` -eq ${#_connect_key} ]; then
+#  curl --tlsv1.2 --silent --show-error --header \
+#  "x-connect-key: ${_connect_key}" \
+#  https://kickstart.jumpcloud.com/Kickstart | sudo bash
+#else
+#  echo -e "\nJumpCloud install failed. Manual install is required." && exit 1
+#fi
